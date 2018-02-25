ef9cdb3fe7b3056503425ffff2661e96b925bc07
wangke <wangke@intbee.com>
2018-02-25 21:31:32 +0800

    登陆页面

--- src/components/login_form/index.js
+++ src/components/login_form/index.js
@@ -14,13 +14,39 @@ class LoginForm extends Component{
     }
 
     render(){
+        const {isFetching} = this.props;
+        const {getFieldDecorator} = this.props.form;
         return(
             <div className={css.login}>
                 <div className='head'>
                     <img src="/build/logo.png" alt=""/>
                     <h3>智蜂客管理员</h3>
                 </div>
-
+                <Form   className="login-form">
+                    <FormItem>
+                        {
+                            getFieldDecorator('username',
+                                {
+                                    rules:[{required:true,message:'请输入用户名'}]
+                                })(
+                                <Input addonBefore={<Icon type='user'/>} placeholder='Username'/>
+                            )
+                        }
+                    </FormItem>
+                    <FormItem>
+                        {
+                            getFieldDecorator('password',
+                                {
+                                    rules:[{required:true,message:'请输入密码'}]
+                                })(
+                                <Input addonBefore={<Icon type='lock'/>} placeholder='Password' type='password'/>
+                            )
+                        }
+                    </FormItem>
+                    <FormItem>
+                        <Button type='primary' htmlType='submit' loading={isFetching} className='login-form-button' >{isFetching?'登录中...':'登录'}</Button>
+                    </FormItem>
+                </Form>
             </div>
 
         );
