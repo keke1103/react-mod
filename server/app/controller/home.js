7f382b9fe4ae0fd4b0502275aa65b9ba8df87e26
awko <wangke@intbee.com>
2018-09-18 09:53:46 +0800

    s

--- server/app/controller/home.js
+++ server/app/controller/home.js
@@ -2,9 +2,30 @@
 
 const Controller = require('egg').Controller;
 
+
+
 class HomeController extends Controller {
   async index() {
     this.ctx.body = 'hi, egg';
+  }
+  async getSession(){
+
+    let req = this.ctx.request;
+    let role = req.query.role;
+    console.log(req.header);
+    let action = req.query.action;
+    let result = await this.ctx.curl('http://192.168.3.18/api/backend/v1/auth/session?roles='+role+'&action='+action, {
+      // 自动解析 JSON response
+       dataType: 'json',
+        headers:req.header ,
+      // 3 秒超时
+       timeout: 3000,
+    });
+
+
+
+
+      this.ctx.body = result.data;
   }
 }
 
